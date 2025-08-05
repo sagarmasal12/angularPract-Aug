eea4b7e9d8af72abbdcc5c782871e4c0191e5fad
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-05 09:37:52 +0530

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { UsersComponent } from './Pages/users/users.component';
 
 export const routes: Routes = [
-   
+    {
+        path: "dashboard",
+        component: DashboardComponent
+    },
+    {
+        path:"user",
+        component:UsersComponent
+    }
 ];
