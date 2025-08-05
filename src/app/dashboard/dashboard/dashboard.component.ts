1bd0a08a7ad30cbe4560dc3e718087d293c56bfd
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-05 18:27:28 +0530

    signal example

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  // ✅ Create a signal with initial value 0
+  count = signal(0);
+  // count = continm(0) ;
+
+  // ✅ Method to increase the count
+  increment() {
+    this.count.set(this.count() + 1);
+  }
+
+  // ✅ Method to reset the count
+  reset() {
+    this.count.set(0);
+  }
 }
+function continm(arg0: number) {
+  throw new Error('Function not implemented.');
+}
+
