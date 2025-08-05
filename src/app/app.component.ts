aa5dad05a57018c24dd8a47b9e973b303f681973
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-05 10:18:16 +0530

    render using routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { UsersComponent } from './Pages/users/users.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,UsersComponent,DashboardComponent],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
