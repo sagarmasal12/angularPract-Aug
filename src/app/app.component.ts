76545849a8145e9dfa08474efac4e7e21fb797eb
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-05 09:56:34 +0530

    render componetn with help of app-user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { UsersComponent } from './Pages/users/users.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,UsersComponent,DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
