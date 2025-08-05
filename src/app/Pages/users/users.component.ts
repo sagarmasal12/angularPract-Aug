77deb24b1220ec32c7671918aceb17e6155cbc32
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-05 15:12:23 +0530

    afsda

--- src/app/Pages/users/users.component.ts
+++ src/app/Pages/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+
+   @Input() name: string = '';
+
+
+    show = true;
+  username = 'Sagar';
+
+ 
+  toggleComponent() {
+    this.show = !this.show;
+  }
+  constructor() {
+    console.log('Constructor called');
+  }
+  
+  courseName = "Angular Full course"
+
+  changeCourse(name:string){
+    this.courseName= name;
+    alert(this.courseName)
+    
+  }
+
+
+  ngOnChanges() {
+    console.log('ngOnChanges called - Input changed:', this.name);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called - Component initialized');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called - Component destroyed');
+  }
+
 
   
 }
