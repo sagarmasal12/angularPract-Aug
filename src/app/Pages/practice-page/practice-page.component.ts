611bdb01417d6a2105222eac60fa777d2d76a491
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-08 09:47:03 +0530

    created new component

---
+++ src/app/Pages/practice-page/practice-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-page',
+  imports: [],
+  templateUrl: './practice-page.component.html',
+  styleUrl: './practice-page.component.css'
+})
+export class PracticePageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { UsersComponent } from './Pages/users/users.component';
+import { PracticePageComponent } from './Pages/practice-page/practice-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:"user",
         component:UsersComponent
+    },
+    {
+        path:"practice-page",
+        component:PracticePageComponent
     }
 ];
