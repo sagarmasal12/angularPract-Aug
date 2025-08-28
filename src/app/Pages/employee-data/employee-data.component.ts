201ecd5e46fb653fcf75887adbcd9cdb3f27f391
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-28 09:48:35 +0530

    new crud

---
+++ src/app/Pages/employee-data/employee-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-data',
+  imports: [],
+  templateUrl: './employee-data.component.html',
+  styleUrl: './employee-data.component.css'
+})
+export class EmployeeDataComponent {
+
+}
