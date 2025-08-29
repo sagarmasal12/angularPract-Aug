9ba561591c4a07a35cf1994a4c562e50760a2814
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-29 10:08:29 +0530

    Products Ui done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UsersComponent } from './Pages/users/users.component';
 import { PracticePageComponent } from './Pages/practice-page/practice-page.component';
 import { StudentsComponent } from './Pages/students/students.component';
 import { EmployeeDataComponent } from './Pages/employee-data/employee-data.component';
+import { ProductDataComponent } from './product-data/product-data.component';
 
 export const routes: Routes = [
   {
@@ -25,5 +26,9 @@ export const routes: Routes = [
   {
     path: 'emp-data',
     component: EmployeeDataComponent,
+  },
+  {
+    path: 'product-data',
+    component: ProductDataComponent,
   },
 ];
---
+++ src/app/product-data/product-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-data',
+  imports: [],
+  templateUrl: './product-data.component.html',
+  styleUrl: './product-data.component.css'
+})
+export class ProductDataComponent {
+
+}
