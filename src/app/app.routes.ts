12c2cea5ba4a3ab921a4299f187cfbc9abef01e5
sagarmasal12 <sagarmasal12@gmail.com>
2025-08-31 22:46:34 +0530

    prativce component remove

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { UsersComponent } from './Pages/users/users.component';
-import { PracticePageComponent } from './Pages/practice-page/practice-page.component';
 import { StudentsComponent } from './Pages/students/students.component';
 import { EmployeeDataComponent } from './Pages/employee-data/employee-data.component';
 import { ProductDataComponent } from './product-data/product-data.component';
@@ -14,10 +13,6 @@ export const routes: Routes = [
   {
     path: 'user',
     component: UsersComponent,
-  },
-  {
-    path: 'practice-page',
-    component: PracticePageComponent,
   },
   {
     path: 'student-record',
