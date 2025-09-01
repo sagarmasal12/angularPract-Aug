275d12c3ec55cbe8225f910a4ad4a6412b06ca02
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-01 10:05:00 +0530

    Add restaurant menu component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UsersComponent } from './Pages/users/users.component';
 import { StudentsComponent } from './Pages/students/students.component';
 import { EmployeeDataComponent } from './Pages/employee-data/employee-data.component';
 import { ProductDataComponent } from './product-data/product-data.component';
+import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.component';
 
 export const routes: Routes = [
   {
@@ -25,5 +26,9 @@ export const routes: Routes = [
   {
     path: 'product-data',
     component: ProductDataComponent,
+  },
+  {
+    path: 'restaurant-menu',
+    component: RestaurantMenuComponent,
   },
 ];
