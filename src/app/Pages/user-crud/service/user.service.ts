9054d36c6d4f0f9127d823bbab0059d83d80eccd
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-02 14:58:01 +0530

    Implement user fetching functionality: add UserService to retrieve users and update UserCrudComponent to display user list

--- src/app/Pages/user-crud/service/user.service.ts
+++ src/app/Pages/user-crud/service/user.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class UserService {
   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
   constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(this.apiUrl);
+  }
 }
--- src/app/Pages/user-crud/user-crud.component.ts
+++ src/app/Pages/user-crud/user-crud.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
+import { IUser } from './model/user.model';
+import { NgForOf } from '../../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-user-crud',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './user-crud.component.html',
-  styleUrl: './user-crud.component.css'
+  styleUrl: './user-crud.component.css',
 })
-export class UserCrudComponent {
+export class UserCrudComponent implements OnInit {
+  users: IUser[] = [];
 
+  constructor(private userSrv: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    return this.userSrv.getUsers().subscribe({
+      next: (res) => (this.users = res),
+      error: (err) => console.error('Error fetching here:- ', err),
+    });
+  }
 }
