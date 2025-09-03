bb2d2baf88838607246ac6ced57644d82be5489b
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-03 14:50:04 +0530

    adsf

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { NgForOf } from '../../../../node_modules/@angular/common/common_module.d-NEF7UaHr';
-import { NgFor } from '@angular/common';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
-  imports: [NgFor],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  itemList = ['sam', 'vijay'];
 }
