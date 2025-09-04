c8d6c9ec8ba00b2a332863342d1ef13be2f07a7f
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-04 14:40:46 +0530

    Add BankloanService with API integration for fetching loan users

--- src/app/Pages/bankloan/bankloan.service.ts
+++ src/app/Pages/bankloan/bankloan.service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ibankloan } from './bankload.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BankloanService {
-  constructor() {}
+  apiUrl = 'https://projectapi.gerasim.in/api/BankLoan/GetAllUsers';
+
+  private bankloan: Ibankloan[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getloanUser(): Observable<Ibankloan[]> {
+    return this.http.get<Ibankloan[]>(this.apiUrl);
+  }
 }
