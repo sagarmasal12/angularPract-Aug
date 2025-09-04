9872d77ce515e5bb6773ae0ba9450733f4612661
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-04 14:40:38 +0530

    Implement BankloanComponent with form handling and data fetching

--- src/app/Pages/bankloan/bankloan.component.ts
+++ src/app/Pages/bankloan/bankloan.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ibankloan } from './bankload.model';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { BankloanService } from './bankloan.service';
+
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-bankloan',
-  imports: [],
+  imports: [NgFor, FormsModule, ReactiveFormsModule],
   templateUrl: './bankloan.component.html',
-  styleUrl: './bankloan.component.css'
+  styleUrl: './bankloan.component.css',
 })
-export class BankloanComponent {
+export class BankloanComponent implements OnInit {
+  bankloan: Ibankloan[] = [];
 
+  bankloanform!: FormGroup;
+
+  error: string = '';
+
+  constructor(private fb: FormBuilder, private bankSrv: BankloanService) {}
+
+  ngOnInit(): void {
+    this.bankloanform = this.fb.group({
+      userId: [],
+      userName: ['', Validators.required],
+      emailId: ['', [Validators.required, Validators.email]],
+      fullName: [''],
+      role: [''],
+    });
+
+    this.getbankLoanuser();
+  }
+
+  getbankLoanuser() {
+    this.bankSrv.getloanUser().subscribe({
+      next: (res: any) => {
+        this.bankloan = res.data;
+
+        console.log('result:-', res.data);
+      },
+      error: (err) => {
+        (this.error = 'Failed to fetch bankloan users'), err;
+      },
+    });
+  }
 }
