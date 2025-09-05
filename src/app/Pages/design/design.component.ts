b620a131a5c15a0cd5156dc8d32e2d4acd55d2ea
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-05 12:51:42 +0530

    Replace placeholder text with structured HTML layout and image bindings in DesignComponent

--- src/app/Pages/design/design.component.ts
+++ src/app/Pages/design/design.component.ts
@@ -4,8 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-design',
   imports: [],
   templateUrl: './design.component.html',
-  styleUrl: './design.component.css'
+  styleUrls: ['./design.component.css'],
 })
 export class DesignComponent {
-
+  imagePath = 'assets/images/bg-top.svg';
 }
