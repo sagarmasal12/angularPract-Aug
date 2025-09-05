3fbcd89c9d7d8773db8311318c6b492ef448d0e3
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-05 11:08:56 +0530

    Refactor BankloanService to streamline API URL and improve method structure

--- src/app/Pages/bankloan/bankloan.service.ts
+++ src/app/Pages/bankloan/bankloan.service.ts
@@ -7,13 +7,29 @@ import { map, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class BankloanService {
-  apiUrl = 'https://projectapi.gerasim.in/api/BankLoan/GetAllUsers';
+  apiUrl = 'https://projectapi.gerasim.in/api/BankLoan';
 
   private bankloan: Ibankloan[] = [];
 
   constructor(private http: HttpClient) {}
 
   getloanUser(): Observable<Ibankloan[]> {
-    return this.http.get<Ibankloan[]>(this.apiUrl);
+    return this.http.get<Ibankloan[]>(`${this.apiUrl}/GetAllUsers`);
+  }
+
+  addbankUsers(res: any) {
+    return this.http.post(
+      'https://projectapi.gerasim.in/api/BankLoan/RegisterCustomer',
+      res,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  onDelete(id: number) {
+    return this.http.delete(
+      'https://projectapi.gerasim.in/api/BankLoan/DeleteUserByUserId/' + id
+    );
+    //  (this.bankloan = this.bankloan.filter((id) => id.userId));
+    // this.getloanUser();
   }
 }
