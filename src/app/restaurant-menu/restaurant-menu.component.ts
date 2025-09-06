633d4d12e22f57d4e5cb974ee20aa5a05d415722
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-06 21:56:17 +0530

    delete component

--- src/app/restaurant-menu/restaurant-menu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-restaurant-menu',
-  imports: [],
-  templateUrl: './restaurant-menu.component.html',
-  styleUrl: './restaurant-menu.component.css',
-})
-export class RestaurantMenuComponent {}
