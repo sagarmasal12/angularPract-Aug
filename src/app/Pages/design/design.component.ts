744142eb6e4570f20dc8546d0dc1de23b14f42d8
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-08 11:16:42 +0530

    toggle

--- src/app/Pages/design/design.component.ts
+++ src/app/Pages/design/design.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-design',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './design.component.html',
   styleUrls: ['./design.component.css'],
 })
 export class DesignComponent {
   imagePath = 'assets/images/bg-top.svg';
+
+  hideDiv: boolean = false;
+
+  toggleDiv(event: any) {
+    // console.log(event);
+    this.hideDiv = event.target.checked;
+  }
 }
