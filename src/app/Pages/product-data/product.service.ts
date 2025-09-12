1d15ba4687ce1de08b496ca4a82d293d9c2cc4ef
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-12 11:24:25 +0530

    product service

---
+++ src/app/Pages/product-data/product.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Productmodel } from './product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService implements OnInit {
+  private product: Productmodel[] = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  saveToStorage() {
+    localStorage.setItem('myproduct', JSON.stringify(this.product));
+  }
+
+  getFromStorage(): Productmodel[] {
+    let temp = localStorage.getItem('myproduct');
+    if (temp) {
+      this.product = JSON.parse(temp);
+      return this.product;
+    }
+    return [];
+  }
+
+  addProduct(obj: Productmodel) {
+    let newId =
+      this.product.length > 0
+        ? Math.max(...this.product.map((s) => s.id ?? 0)) + 1
+        : 1;
+
+    obj.id = newId;
+    this.product.push(obj);
+    this.saveToStorage();
+  }
+
+  updateProd(updateProList: Productmodel) {
+    let index = this.product.findIndex((prod) => prod.id == updateProList.id);
+    if (index >= 0) {
+      this.product = this.getFromStorage();
+      this.product[index] = updateProList;
+      this.saveToStorage();
+      console.log('updated arrya', this.product);
+    } else {
+      alert('Invalid Status');
+    }
+  }
+
+  delete(ID: number) {
+    this.product = this.product.filter((prod) => prod.id !== ID);
+    this.saveToStorage();
+  }
+}
